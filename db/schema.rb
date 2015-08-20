119e99796a146f1ff7d42a17bd3d2ff6786ff63f
jamesyu <zetaentranceserious@gmail.com>
2015-08-20 14:39:06 +0800

    Comment associations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820054613) do
+ActiveRecord::Schema.define(version: 20150820061912) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "body"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
