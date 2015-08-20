fd5c7249f575c1a4d35c8a593883e06d4203a358
jamesyu <zetaentranceserious@gmail.com>
2015-08-20 14:44:16 +0800

    Category model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820061912) do
+ActiveRecord::Schema.define(version: 20150820064003) do
+
+  create_table "categories", force: :cascade do |t|
+  end
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
