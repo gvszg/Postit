92d1f76c345cac7eaec0251e65852d0bfec4e9a5
jamesyu <zetaentranceserious@gmail.com>
2015-08-26 16:13:05 +0800

    Register new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_secure_password # it seems no password_confirmation by default 
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, length: {minimum: 6}
 end
