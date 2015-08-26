1f4da03ff1e0272aef18f0e5dc03bd905716b6b3
jamesyu <zetaentranceserious@gmail.com>
2015-08-26 11:13:52 +0800

    Have user password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820074417) do
+ActiveRecord::Schema.define(version: 20150826022531) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20150820074417) do
     t.string   "username"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
