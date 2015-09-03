6e038a5ebadd8003e14a3303f494ec28b1f7f72b
jamesyu <zetaentranceserious@gmail.com>
2015-09-03 17:41:35 +0800

    Add slug to users and categories

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,14 @@ class User < ActiveRecord::Base
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 6}
+
+  before_save :generate_slug
+
+  def to_param
+    self.slug
+  end
+
+  def generate_slug
+    self.slug = self.username.gsub(" ", "-").downcase
+  end
 end
