2371f356ebdc2f8cc92c3ccd6bdeb832604368cb
jamesyu <zetaentranceserious@gmail.com>
2015-09-05 11:39:51 +0800

    Admin Role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,15 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :comments
   has_many :votes
+  
   has_secure_password # it seems no password_confirmation by default 
 
   validates :username, presence: true, uniqueness: true
   validates :password, presence: true, on: :create, length: {minimum: 6}
 
   sluggable_column :username
+
+  def admin?
+    self.role == 'admin'
+  end
 end
